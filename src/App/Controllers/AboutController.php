57891c131119dcba088f0645c34f4e4cbc8a0c8c
camtonguyen <nguyenthicamto@gmail.com>
2026-07-24 12:21:16 +0700

    update containers & dependency injection

--- src/App/Controllers/AboutController.php
+++ src/App/Controllers/AboutController.php
@@ -11,12 +11,8 @@ use App\Config\Paths;
 
 class AboutController
 {
-  public TemplateEngine $view;
 
-  public function __construct()
-  {
-    $this->view = new TemplateEngine(Paths::VIEW);
-  }
+  public function __construct(private TemplateEngine $view) {}
 
   public function about()
   {
